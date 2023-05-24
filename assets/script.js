ea44ab4acd990d9bbcb399969e8bbaaa3079c7a7
keurid <kevindesantiago99@gmail.com>
2023-05-24 12:57:36 -0700

    added an array for questions needed to be used

---
+++ assets/script.js
@@ -0,0 +1,57 @@
+var questions = [
+    {
+    numb: 1,
+    question: "Which is not a JavaScript Data Type?",
+    answer: "Hoolean",
+    options: [
+      "Number",
+      "String",
+      "Object",
+      "Hoolean"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which company developed JavaScript?",
+    answer: "Netscape",
+    options: [
+      "Java",
+      "Mojang",
+      "Oracle",
+      "Netscape"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which is not a type of Pop up box?",
+    answer: "Caution",
+    options: [
+      "Prompt",
+      "Confirm",
+      "Alert",
+      "Caution"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which is not a type of error?",
+    answer: "Screentime Error",
+    options: [
+      "Screentime Error",
+      "Load time errors",
+      "Runtime errors",
+      "Logical errors"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which is not a Loop?",
+    answer: "infinite loop",
+    options: [
+      "for loop",
+      "while loop",
+      "do…while loop",
+      "infinite loop"
+    ]
+  },
+];
